c0dff8fd79b47b4281bcdb7082def1132b18d095
QL-Arpit <arpit@quokkalabs.com>
2025-09-17 10:09:26 +0530

    Update VideoIntegrationPage to serve video.html instead of index.html

--- frontend/src/app/video/page.tsx
+++ frontend/src/app/video/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function VideoIntegrationPage() {
     // Serve the static index.html from Next.js public folder
-    const src = "/index.html"; // now using bundled public asset
+    const src = "/video.html";
     return (
         <div className="flex flex-col w-full h-[calc(100vh-2rem)] p-4 gap-4">
 
